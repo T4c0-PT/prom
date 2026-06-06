347f82f4cb4e619918949de4da83e9d11b4a9c8b
T4c0-PT <danielpp1801@gmail.com>
2026-06-05 21:07:24 -0500

    upload 3

--- Assets/scripts/database/UserData.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-[Serializable]
-public class UserData
-{
-    public string name;
-    public int score;
-}
--- Assets/scripts/database/UserResultData.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-[Serializable]
-public class UserResultData
-{
-    public UserData[] data;
-}
